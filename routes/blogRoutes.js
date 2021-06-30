b52ede18c01872260722b1d0d8498d741b881a9a
hamzakamboh14 <hamza21461@gmail.com>
2021-06-30 14:06:06 +0500

    edit image done

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -8,7 +8,7 @@ route.get('/create',blogController.blog_create_get)
 route.post('/',upload.single('image'),blogController.blog_create_post)
 route.get('/single/:id',blogController.blog_single)
 route.get('/edit/:id',blogController.blog_edit)
-route.post('/update/:id',blogController.blog_update)
+route.post('/update/:id',upload.single('image'),blogController.blog_update)
 route.get('/delete/:id',blogController.blog_delete)
 
 module.exports = route
